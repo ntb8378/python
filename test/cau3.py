47df29fce600b8ddc95e823e6e006cbb5e37d6f7
Nguyễn Thiên Bảo <ntb8378@gmail.com>
2026-05-28 07:48:17 +0700

    baitap

--- test/cau3.py
+++ test/cau3.py
@@ -1,11 +1,11 @@
 
 # câu 3
 sum_ok_item= 0
-chose=''
 total_item=0
-while (chose == '0'):
-    print('nhập 0 để thoát' \
-    '      nhập 1 để nhập')
+while True:
+    print('nhập 0 để thoát, \
+           nhập 1 để nhập')
+    chose= input('mời nhập:')
     if(chose == '1'):
             thunghang= int(input('nhập số thùng hàng:'))
             for i in range(1,thunghang+1):
@@ -13,8 +13,9 @@ while (chose == '0'):
                 if(motthung >= 0 ):
                     sum_ok_item +=1
                     total_item = total_item +motthung
+            print(f'tổng số lượng thùng hàng hợp lệ: {sum_ok_item}')
+            print(f'tổng số lượng sản phẩm thu được: {total_item}')
     if(chose == '0'):
         print('thoát')
         break
-print(f'tổng số lượng thùng hàng hợp lệ: {sum_ok_item}')
-print(f'tổng số lượng sản phẩm thu được: {total_item}')
+    
